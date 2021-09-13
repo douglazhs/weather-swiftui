2fe293ac76ab846aeac6d5f598b16e7c40da9d84
Douglas Henrique de Souza Pereira <50143403+douglazhs@users.noreply.github.com>
2021-09-13 16:29:46 -0300

    Created the initial project

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Douglas Henrique de Souza Pereira on 13/09/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
